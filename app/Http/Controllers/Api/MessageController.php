8a128a1fb85fb92ce2858a509d87b01d5712d694
Sistemas Universidad Tecnológica de Chetumal <william.sanchez@utchetumal.edu.mx>
2021-11-12 03:31:20 -0500

    commit inicial

---
+++ app/Http/Controllers/Api/MessageController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mensajes = Message::all();
+
+        return $mensajes;
+        //return response()->json($mensajes);
+        /*return response()->json(
+            array ( 'data' => $mensajes,
+                    'code' => '200',
+                    'msj' => ''
+        ));*/
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /*var_dump($request);
+        exit;*/
+        $mensaje = new Message();
+        $mensaje->user = $request->input('user');
+        $mensaje->message = $request->input('message');
+
+        $mensaje->save();
+
+        $data = [
+            'user' => $request->input('user'),
+            'message' => $request->input('message'),
+        ];
+
+        //return $mensaje;
+        return response()->json($mensaje);
+        /*return response()->json(
+            array ( 'data' => $mensaje,
+                    'code' => '200',
+                    'msj' => ''
+        ));*/
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Message $message)
+    {
+        //
+    }
+}
