721320ef0314f4ae0c5d31d08a12ceabfef3cd8f
William Sanchez <william.sanchez@utchetumal.edu.mx>
2022-09-27 14:42:32 -0500

    Forzar https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app_env')==='production'){
+        if(config('APP_ENV')==='production'){
             URL::forceScheme('https');
         }
     }
