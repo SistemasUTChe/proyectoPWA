bc36eab53bbb826821179f06ee58ee09c835b09b
William Sanchez <william.sanchez@utchetumal.edu.mx>
2022-10-19 17:39:24 -0500

    Guardar cache

--- public/services.js
+++ public/services.js
@@ -7,34 +7,26 @@
     
     var APP_SHELL = [
         '/',
-        '/offline.html'
+        'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap',
+        '/offline.html',
+        '/no-img.jpg'
     ];
     
     
     function limpiarCache( cacheName, numeroItems ) {
-    
-    
         caches.open( cacheName )
             .then( cache => {
-    
                 return cache.keys()
                     .then( keys => {
-                        
                         if ( keys.length > numeroItems ) {
                             cache.delete( keys[0] )
                                 .then( limpiarCache(cacheName, numeroItems) );
                         }
                     });
-    
-                
             });
     }
     
-    
-    
-    
     self.addEventListener('install', e => {
-    
     
         const cacheProm = caches.open( CACHE_STATIC_NAME )
             .then( cache => {
@@ -45,51 +37,46 @@
             });
     
         const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
-                .then( cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
-    
+                .then( cache => {
+                    cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+                    cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
+                });
     
         e.waitUntil( Promise.all([cacheProm, cacheInmutable]) );
     
     });
     
-    
     self.addEventListener('activate', e => {
     
-    
         const respuesta = caches.keys().then( keys => {
-    
             keys.forEach( key => {
-    
                 if (  key !== CACHE_STATIC_NAME && key.includes('static') ) {
                     return caches.delete(key);
                 }
-    
             });
-    
         });
-    
-    
     
         e.waitUntil( respuesta );
     
     });
     
-    
-    
-    
-    
     self.addEventListener('fetch', e => {
-    
-    
+
         // 2- Cache with Network Fallback
         const respuesta = caches.match( e.request )
             .then( res => {
-    
                 if ( res ) return res;
     
-                // No existe el archivo
-    
+                // No existe el archivo EN CACHE
                 return fetch( e.request ).then( newResp => {
+
+                    if ( !newResp.ok ) {
+                        //Sin red
+                        if ( /\.(png|jpg|jpeg)$/i.test( e.request.url ) ) {
+                            return caches.match('no-img.jpg');
+                        }    
+                        console.log(e.request.url);
+                    }
     
                     caches.open( CACHE_DYNAMIC_NAME )
                         .then( cache => {
@@ -100,76 +87,17 @@
                     return newResp.clone();
                 })
                 .catch( err => {
-    
-                    if ( e.request.headers.get('accept').includes('text/html') ) {
-                        return caches.match('offline.html');
-                    }
-    
-                    if ( /\.(png|jpg)$/i.test( e.request.url ) ) {
-    
+                    if ( /\.(png|jpg|jpeg)$/i.test( e.request.url ) ) {
                         return caches.match('no-img.jpg');
                     }    
-    
-                
+
+                    console.log(e.request.url);
                 });
-    
     
             });
     
-    
-    
-    
         e.respondWith( respuesta );
-    
-    
-    
+ 
     });
 
-
-
-
-
-
-
-
-    // Instalar en caché
-  /*self.addEventListener("install", event => {
-    console.log("SW: Instalación - inicio");
-      this.skipWaiting();
-      event.waitUntil(
-          caches.open(staticCacheName)
-              .then(cache => {
-                  return cache.addAll(filesToCache);
-              })
-      )
-      console.log("SW: Instalación - final");
-  });
-
-  // Limpiar caché y activar
-  self.addEventListener('activate', event => {
-      event.waitUntil(
-          caches.keys().then(cacheNames => {
-              return Promise.all(
-                  cacheNames
-                      .filter(cacheName => (cacheName.startsWith("pwa-")))
-                      .filter(cacheName => (cacheName !== staticCacheName))
-                      .map(cacheName => caches.delete(cacheName))
-              );
-          })
-      );
-  });*/
-
-  // Revisar caché cuando no hay conexión
-  self.addEventListener("fetch", event => {
-      event.respondWith(
-          caches.match(event.request)
-              .then(response => {
-                  console.log(event.request);
-                  return response || fetch(event.request);
-              })
-              .catch(() => {
-                  console.log("No encontrado", event.request);
-                  //return caches.match('/offline.html');
-              })
-      )
-  });
+   
