72ed7be637e06fa4a192a955a309ba690d00a609
William Sanchez <william.sanchez@utchetumal.edu.mx>
2022-10-28 14:12:55 -0500

    Agregar mensajero

---
+++ public/js/app.js
@@ -0,0 +1,214 @@
+
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
+
+if ( navigator.serviceWorker ) {
+
+
+    if ( url.includes('localhost') ) {
+        swLocation = '/sw.js';
+    }
+
+
+    navigator.serviceWorker.register( swLocation );
+}
+
+
+
+
+
+// Referencias de jQuery
+
+var titulo      = $('#titulo');
+var nuevoBtn    = $('#nuevo-btn');
+var salirBtn    = $('#salir-btn');
+var cancelarBtn = $('#cancel-btn');
+var postBtn     = $('#post-btn');
+var avatarSel   = $('#seleccion');
+var timeline    = $('#timeline');
+
+var modal       = $('#modal');
+var modalAvatar = $('#modal-avatar');
+var avatarBtns  = $('.seleccion-avatar');
+var txtMensaje  = $('#txtMensaje');
+
+// El usuario, contiene el ID del héroe seleccionado
+var usuario;
+
+// ===== Codigo de la aplicación
+
+function crearMensajeHTML(mensaje, personaje) {
+
+    var content =`
+    <li class="animated fadeIn fast">
+        <div class="avatar">
+            <img src="img/avatars/${ personaje }.jpg">
+        </div>
+        <div class="bubble-container">
+            <div class="bubble">
+                <h3>@${ personaje }</h3>
+                <br/>
+                ${ mensaje }
+            </div>
+            
+            <div class="arrow"></div>
+        </div>
+    </li>
+    `;
+
+    timeline.prepend(content);
+    cancelarBtn.click();
+
+}
+
+
+
+// Globals
+function logIn( ingreso ) {
+
+    if ( ingreso ) {
+        nuevoBtn.removeClass('oculto');
+        salirBtn.removeClass('oculto');
+        timeline.removeClass('oculto');
+        avatarSel.addClass('oculto');
+        modalAvatar.attr('src', 'img/avatars/' + usuario + '.jpg');
+    } else {
+        nuevoBtn.addClass('oculto');
+        salirBtn.addClass('oculto');
+        timeline.addClass('oculto');
+        avatarSel.removeClass('oculto');
+
+        titulo.text('Seleccione Personaje');
+    
+    }
+
+}
+
+
+// Seleccion de personaje
+avatarBtns.on('click', function() {
+
+    usuario = $(this).data('user');
+
+    titulo.text('@' + usuario);
+
+    logIn(true);
+
+});
+
+// Boton de salir
+salirBtn.on('click', function() {
+
+    logIn(false);
+
+});
+
+// Boton de nuevo mensaje
+nuevoBtn.on('click', function() {
+
+    timeline.addClass('oculto');
+    modal.removeClass('oculto');
+    //modal.show();
+
+    modal.animate({ 
+        marginTop: '-=1000px',
+        opacity: 1
+    }, 200 );
+
+});
+
+// Boton de cancelar mensaje
+cancelarBtn.on('click', function() {
+   modal.animate({ 
+       marginTop: '+=1000px',
+       opacity: 0
+    }, 200, function() {
+        modal.addClass('oculto');
+        txtMensaje.val('');
+    });
+});
+
+// Boton de enviar mensaje
+postBtn.on('click', function() {
+
+    var mensaje = txtMensaje.val();
+    if ( mensaje.length === 0 ) {
+        cancelarBtn.click();
+        return;
+    }
+
+    var data = {
+        message: mensaje,
+        user: usuario
+    };
+
+    console.log(data);
+
+    fetch('https://www.pruebas-utche.website/api/messages', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( data )
+    })
+    .then( res => res.json() )
+    .then( res => console.log( 'app.js', res ))
+    .catch( err => console.log( 'app.js error:', err ));
+
+    logIn(true);
+
+    crearMensajeHTML( mensaje, usuario );
+
+});
+
+// Obtener mensajes del servidor
+function getMensajes() {
+
+    fetch('https://www.pruebas-utche.website/api/messages')
+        .then( res => res.json() )
+        .then( respuesta => {
+            console.log(respuesta);
+            posts = respuesta;
+            console.log(posts);
+            posts.forEach( post =>
+                crearMensajeHTML( post.message, post.user ));
+
+
+        });
+
+
+}
+
+//getMensajes();
+
+// Detectar cambios de conexión
+function isOnline() {
+
+    if ( navigator.onLine ) {
+        // tenemos conexión
+         alert('online');
+/*        $.mdtoast('Online', {
+            interaction: true,
+            interactionTimeout: 1000,
+            actionText: 'OK!'
+        });*/
+
+
+    } else{
+        // No tenemos conexión
+        alert('offline');
+        /*$.mdtoast('Offline', {
+            interaction: true,
+            actionText: 'OK',
+            type: 'warning'
+        });*/
+    }
+
+}
+
+window.addEventListener('online', isOnline );
+window.addEventListener('offline', isOnline );
+
+//isOnline();
+
