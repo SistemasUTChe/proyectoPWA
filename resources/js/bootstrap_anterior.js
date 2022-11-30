5daedb3a51faa86497ad3e5f5df975b2dbf620b4
William Sanchez <william.sanchez@utchetumal.edu.mx>
2022-11-30 11:00:30 -0500

    Mensajero para producción

--- resources/js/bootstrap_anterior.js
+++ resources/js/bootstrap_anterior.js
@@ -1,5 +1,7 @@
 window._ = require('lodash');
 
+<<<<<<< HEAD
+=======
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -19,23 +21,8 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+>>>>>>> usuarios
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
